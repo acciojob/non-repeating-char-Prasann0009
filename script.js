d6f8317121c0d398250f0b7014d43988e38e8fb7
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-09-15 12:09:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-}
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+    // Write your code here0
+    let prev=str.charAt(0);
+    let curr=str.charAt(0);
+    let count=1;
+    let present=false;
+       for(let i=0;i<=str.length;i++)
+       {
+        curr=str.charAt(i);
+         if(curr==prev)
+         {
+            count++;
+            continue;
+         }
+         else
+         {
+            if(count==1)
+            {
+                console.log(prev);
+                present=true;
+                break;
+            }
+            else
+            {
+                count=1;
+                prev=curr;
+            }
+         }
+       }
+       if(present==false)
+       console.log("null");
+   }
+   const input = prompt("Enter a string");
+   alert(firstNonRepeatedChar(input)); 
